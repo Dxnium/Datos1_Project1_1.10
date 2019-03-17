913dba01ac8beea256a0383f87757db3a711f43b
Kevin <kabarrantes163@gmail.com>
2019-03-16 22:02:03 -0600

    Agregacion clase square
    La clase Square sirve como posicion en la matriz de tablero, contiene un multiplicador y un atributo en el que se guardan fichas de juego

---
+++ src/game/logic/Square.java
@@ -0,0 +1,37 @@
+package programlogic;
+
+public class Square {
+	
+	private LetterTile letter;
+	private int multiplier;
+	private boolean activeFlag = false;
+	
+	public Square(int multiplier){
+		if ( multiplier != 0) {
+			this.setActiveFlag(true);
+		}
+		this.multiplier = multiplier;
+		
+	}
+	
+	public LetterTile getLetter() {
+		return letter;
+	}
+	public void setLetter(LetterTile letter) {
+		this.letter = letter;
+	}
+	public int getMultiplier() {
+		return multiplier;
+	}
+	public void setMultiplier(int multiplier) {
+		this.multiplier = multiplier;
+	}
+	public boolean isActiveFlag() {
+		return activeFlag;
+	}
+	public void setActiveFlag(boolean activeFlag) {
+		this.activeFlag = activeFlag;
+	}
+	
+
+}
