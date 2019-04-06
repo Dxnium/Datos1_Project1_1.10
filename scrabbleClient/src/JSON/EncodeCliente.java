dd53833b418ef851f3041df0047f07f3f94219f4
Dxnium <danielum99@gmail.com>
2019-04-06 12:09:16 -0600

    Sockets
    Conexion estre diferentes computadoras

---
+++ scrabbleClient/src/JSON/EncodeCliente.java
@@ -0,0 +1,7 @@
+package JSON;
+
+public class EncodeCliente extends Encode{
+	public EncodeCliente() {
+		super();
+	}
+}
--- scrabbleServer/sockets/Server.java
+++ scrabbleServer/sockets/Server.java
@@ -4,19 +4,27 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import org.json.simple.JSONArray;
+
+import JSON.Encode;
+import JSON.Juego;
 
 public class Server implements Runnable  {
 	ServerSocket server;  
 	Socket socket; 
 	final int port = 9999;
+	
 
 	@Override
 	public void run() {
 		try {
 			//server created in port 9999
-			server = new ServerSocket(port);
+			server = new ServerSocket( port);
 			System.out.println("New server started");	
 			socket = server.accept();//Waiting for a client 
 			
@@ -45,6 +53,16 @@ public class Server implements Runnable  {
 	}
 
 	private String GetJMensaje() {
-		return "No hay mensaje";
+		Encode datos = new Encode();
+		//Crea el arreglo con los datos de la Clase 
+		JSONArray arr = datos.arrayData(new Juego("Daniel",22));
+		Writer out = new StringWriter();//crear un variable de tipo Writer para almacenar el array y poder mostarlo en pantalla 
+		try {
+			arr.writeJSONString(out); //guardar el JSONArray en un string 
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		}String out1 = out.toString();
+		return out1;
 	}
 }
