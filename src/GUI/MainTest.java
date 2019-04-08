b7feb0e4ae91cef583538750daaa79eda91af9f0
Alejandro Loría J <33795286+aleloria@users.noreply.github.com>
2019-04-08 01:09:52 -0600

    Add files via upload
    Se agregan las ventanas de inicio, falta manejar las variables que manejan la información que solicita cada paso

--- src/GUI/MainTest.java
+++ src/GUI/MainTest.java
@@ -3,15 +3,23 @@ package GUI;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.net.URISyntaxException;
 
-import Sockets.Server;
+//import Sockets.Server;
 
 public class MainTest {
 
 
 	public static void main(String[] args) throws FileNotFoundException {
-		StartWindow inicio = new StartWindow();	
+		//StartWindow inicio = new StartWindow();
+		Vent_Inicio frame = new Vent_Inicio();  //Esta linea y la siguiente permiten probar las ventanas de inicio
+		frame.setVisible(true);
+//		Ventana2 ventana2 = new Ventana2();
+//		String dato = "[{\"Name\":\"Daniel\",\"Fichas\":\"22\"}]" ; 
+//		String[] dato2 = dato.split(",") ;
+//		for(String i:dato2) {
+//			System.out.println(i);	
+//		}
+//		
 }
 	
 }
