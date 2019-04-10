76ba89eb22dda1ca615be59abfea1e61e03fcbe8
Kevin <kabarrantes163@gmail.com>
2019-04-09 21:25:44 -0600

    commit estructura
    agregue una lista simple necesaria para el manejo de la matriz

--- src/game/logic/PlayedWordList.java
+++ src/game/logic/PlayedWordList.java
@@ -1,5 +1,54 @@
 package game.logic;
 
 public class PlayedWordList {
+	WordListNode head;
 
+	
+	
+	
+
+
+
+	public void append (String data) {//adds a node at the end of the list
+		if (head == null) {
+			this.head = new WordListNode (data);
+			return;
+		}
+
+		WordListNode current = head;
+		while (current.getNext() != null) {
+			current = current.getNext();
+		}current.setNext( new WordListNode(data)) ;
+
+	}
+
+
+	public void eraseData(String data) {//erases a node using a search criteria
+		if (head == null) return;
+		if (head.getData()==data) {
+			this.setHead(head.getNext());
+		}
+
+		WordListNode current = head;
+		while (current.getNext() != null) {
+			if(current.getNext().getData()==data) {
+				current.setNext(current.getNext().getNext());
+				return;
+			}
+			current = current.getNext();
+
+		}
+	}
+
+
+	public void setHead(WordListNode head) {
+		this.head = head;
+	}
+
+	public WordListNode getHead() {
+		return head;
+	}
+	
 }
+	
+
--- src/game/logic/WordListNode.java
+++ src/game/logic/WordListNode.java
@@ -1,5 +1,32 @@
 package game.logic;
 
 public class WordListNode {
+	
+	String data;
+	WordListNode next;
+	
+	public String getData() {
+		return data;
+	}
+
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+
+	public WordListNode getNext() {
+		return next;
+	}
+
+
+	public void setNext(WordListNode next) {
+		this.next = next;
+	}
+
+
+	public WordListNode(String data) {
+		this.data=data;
+	}
 
 }
