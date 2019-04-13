757b7cedbdc938d578f2d5724df13c038e4e8887
aleloria <aloria@cedesdonbosco.ed.cr>
2019-04-12 22:31:22 -0600

    Se agregam java.doc a la clase SMS

--- src/com/twilio/SMS.java
+++ src/com/twilio/SMS.java
@@ -7,14 +7,22 @@ import com.twilio.type.PhoneNumber;
 // TODO: Auto-generated Javadoc
 /**
  * The Class SMS.
+ * Esta clase envia un SMS a un número telefonico
+ * mediante el API de Twilio, para lo cual se debio instalar
+ * un Jar con las librerias correspondientes.
  */
 public class SMS {
 
-	/** The Constant ACCOUNT_SID. */
+	/** The Constant ACCOUNT_SID. 
+	 * Esta variable almacena el ID para ingresar a Twilio
+	 * */
+	
 	// Find your Account Sid and Token at twilio.com/user/account
 	  public static final String ACCOUNT_SID = System.getenv("Twilio_SID");
 	  
-  	/** The Constant AUTH_TOKEN. */
+  	/** The Constant AUTH_TOKEN. 
+  	 * Esta variable almacena el Token para la autenticación en Twilio.
+  	 * */
   	public static final String AUTH_TOKEN = System.getenv("Twilio_Token");;
 	  //public String MensajeSMS;
 	  //public String NumeroDestino;
@@ -24,6 +32,9 @@ public class SMS {
   	 *
   	 * @param MensajeSMS the mensaje SMS
   	 * @param NumeroDestino the numero destino
+  	 * 
+  	 * Este método se encarga se hacer el envio del SMS. Recibi como parametros dos String,
+  	 * uno con el Mensaje deseado y otro con el Numero de Telefono al que se desea enviar el SMS.
   	 */
   	public void EnvioSMS(String MensajeSMS, String NumeroDestino) {
 	    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
